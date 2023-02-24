1c2c80869f0148812b14836dbfe591eb652f70a5
zsh-lalala <3511780967@qq.com>
2023-02-24 11:07:25 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'amfe-flexible';
+import { Button } from 'vant';
 
 createApp(App).mount('#app')
+
+const app =createApp(App);
+app.use(Button)
